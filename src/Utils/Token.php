7b3231bffca37aa27f3fe9986624a86cf3d6770d
AC - DC IDN (IDCH) <dimss.satria@gmail.com>
2024-10-03 11:10:50 +0000

    remove duplicate function verifiy

--- src/Utils/Token.php
+++ src/Utils/Token.php
@@ -146,19 +146,4 @@ class Token
             return Response::error($e->getMessage(), $e->getCode());
         }
     }
-
-    public static function verify(string $token): array
-    {
-        try {
-            // Verifikasi Token
-            $decoded = JWT::decode($token, new Key(env("JWT_KEY"), "HS256"));
-            return json_decode(json_encode($decoded), true);
-        } catch (SignatureInvalidException $e) {
-            return Response::error($e->getMessage(), 4001);
-        } catch (ExpiredException $e) {
-            return Response::error($e->getMessage(), 4002);
-        } catch (\Exception $e) {
-            return Response::error($e->getMessage(), $e->getCode());
-        }
-    }
 }
